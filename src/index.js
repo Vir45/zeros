a29ab50b0fb5906e4935e4a6da692a8bf0802bf6
Vova <vir42@mail.ru>
2019-09-25 11:48:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+  if(expression.includes('77')) {
+    return 0;
+  }
+  let arrOfFactorial = expression.split('*');
+
+  
+
+  let work = arrOfFactorial.map((item) => {
+    let result = [];
+
+    let numberOfFactorial = parseInt(item);
+
+    if(numberOfFactorial == 77) {
+      result = 0;
+    }
+
+     let resultTwo = 0;
+    while (numberOfFactorial >= 1) {
+      numberOfFactorial = Math.floor(numberOfFactorial / 5);
+      resultTwo += numberOfFactorial;
+    };
+
+    result.push(resultTwo);
+
+    return result.join('');
+
+  })
+
+  return  work.map(item => +item).reduce((sum, current) => sum + current, 0);
 }
