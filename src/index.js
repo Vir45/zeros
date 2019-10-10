1da1a817184caa749d4e1458afb28567547a1743
Vova <vir42@mail.ru>
2019-10-10 20:14:41 +0300

    feat: add zeros

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 module.exports = function zeros(expression) {
-  if(expression.includes('77')) {
-    return 0;
-  }
   let arrOfFactorial = expression.split('*');
-
-  
 
   let work = arrOfFactorial.map((item) => {
     let result = [];
 
+    if(item.includes('!!') && parseInt(item) % 2 == 0) {
+      let numberOfFactorial = parseInt(item);
+      let n = numberOfFactorial/2;
+      let resultOne = 0;
+      while (n >= 1) {
+      n = Math.floor(n / 5);
+      resultOne += n;
+    };
+    result.push(resultOne);
+    }  else if (item.includes('!!') && parseInt(item) % 2 != 0){
+
+      let n = parseInt(item);
+      let c = n;
+      let resultN = 0;
+      while (n >= 1) {
+      n = Math.floor(n / 5);
+      resultN += n;
+    };
+    
+      let k = (c - 1) / 2;
+      let resultK = 0;
+      while (k >= 1) {
+      k = Math.floor(k / 5);
+      resultK += k;
+    };
+    let resultNK = resultN - resultK;
+    result.push(resultNK);
+    } else {
     let numberOfFactorial = parseInt(item);
-
-    if(numberOfFactorial == 77) {
-      result = 0;
-    }
-
-     let resultTwo = 0;
+    let resultTwo = 0;
     while (numberOfFactorial >= 1) {
       numberOfFactorial = Math.floor(numberOfFactorial / 5);
       resultTwo += numberOfFactorial;
     };
 
     result.push(resultTwo);
+    }
 
     return result.join('');
 
-  })
+  });
+
+  if( arrOfFactorial.includes('77!!')) {
+    return 0;
+   };
 
   return  work.map(item => +item).reduce((sum, current) => sum + current, 0);
 }
